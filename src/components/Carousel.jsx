0cf8d84bd447612144a70b0225e3e4364e105bee
Salman-sajib <sfsajib@gmail.com>
2024-12-18 20:52:52 +0600

    changed some styles

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi2";
 
 function Carousel({
   children,
   autoPlay = false,
   autoPlayInterval = 3000,
-  showArrows = true,
+  showButtonArrows = true,
   showDots = true,
 }) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -46,20 +46,20 @@ function Carousel({
         ))}
       </div>
 
-      {/* Arrows */}
-      {showArrows && (
+      {/* Buttons Arrows */}
+      {showButtonArrows && (
         <>
           <button
-            className="absolute left-2 top-1/2 -translate-y-1/2 bg-gray-700/50 hover:bg-gray-700 text-white rounded-full p-2 border border-gray-100 transition-all duration-200"
+            className="absolute left-2 top-1/2 -translate-y-1/2 bg-gray-100/10 backdrop-blur-md hover:bg-gray-700 text-gray-50 rounded-full p-2 border border-gray-100/0 hover:border-gray-100 transition-all duration-200"
             onClick={prevSlide}
           >
-            <FaAngleLeft size={22} />
+            <HiChevronDoubleLeft size={22} />
           </button>
           <button
-            className="absolute right-2 top-1/2 -translate-y-1/2 bg-gray-700/50 hover:bg-gray-700 text-white rounded-full p-2 border border-gray-100 transition-all duration-200"
+            className="absolute right-2 top-1/2 -translate-y-1/2 bg-gray-100/10 backdrop-blur-md hover:bg-gray-700 text-gray-50 rounded-full p-2 border border-gray-100/0 hover:border-gray-100 transition-all duration-200"
             onClick={nextSlide}
           >
-            <FaAngleRight size={22} />
+            <HiChevronDoubleRight size={22} />
           </button>
         </>
       )}
